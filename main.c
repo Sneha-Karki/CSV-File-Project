2023a2784a90487682586d648ed40cabbb93cc73
Sneha Karki <sneha.karki@my.utsa.edu>
2024-12-31 14:51:47 -0600

    main.c

--- main.c
+++ main.c
@@ -4,21 +4,17 @@
 #include "route-records.h"
 
 int main(int argc, char *argv[]) {
-    /* 1. Declare variables here */
     FILE *fileIn;
     RouteRecord *records = NULL;
     int actualRecords = 0;
     int menuOption;
     char key1[4], key2[4];
 
-    /* 2. Check command line arguments here. If a command line argument (for the file name) is missing, 
-       print out the following: ERROR: Missing file name and end the program */
     if (argc < 2) {
         printf("ERROR: Missing file name\n");
         return EXIT_FAILURE;
     }
 
-    /* 3. Attempt to open the file. Print out Opening <filename>... before you call fopen(). */
     printf("Opening %s...\n", argv[1]);
     fileIn = fopen(argv[1], "r");
 
@@ -29,29 +25,11 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    /* 5. Do the following to load the records into an array of RouteRecords
-        5.1 Call createRecords(), which will read through the file once to find the total number of 
-            lines/records in the file. Use this count to dynamically allocate memory for an array of 
-            RouteRecords. It returns a pointer to that array. Don't forget to rewind the file pointer.
-        5.2 Call fillRecords() to go through the CSV file again to fill in the values. It will then 
-            return the actual number of items the array has. Recall that not all records in the 
-            original CSV file will be entered into the array. Print the number of unique routes 
-            operated by different airlines: Unique routes operated by airlines: ???
-        5.3 Close the file.
-    */
     records = createRecords(fileIn);
     actualRecords = fillRecords(records, fileIn);
     printf("Unique routes operated by airlines: %d\n", actualRecords);
     fclose(fileIn);
 
-    /* 6. Create an infinite loop that will do the following:
-        6.1 Call printMenu()
-        6.2 Ask the user to input a value for the menu
-        6.3 Handle the case in which a non-integer value is entered
-        6.4 Create a switch/case statement to handle all the menu options
-            6.4.1 Each option requires the user to enter a search key
-            6.4.2 Quit needs to free the array
-    */
     while (1) {
         printMenu();
         if (scanf("%d", &menuOption) != 1) {
